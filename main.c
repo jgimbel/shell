51743c366811b032391d6a4bf22b969d5dfe6dd0
joel gimbel <jgimbel@live.com>
2014-02-24 18:51:40 -0600

    adding files

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string program;
+    progam < cin;
+
+    switch(program){
+    case "cd":
+        break;
+    case "dir":
+        break;
+    case "clr":
+        break;
+    case "environ":
+        break;
+    case "echo":
+        break;
+    case "help":
+        break;
+    case "pause":
+        break;
+    case "quit":
+        break;
+    }
+
+    printf("Hello World!\n");
+    return 0;
+}
+

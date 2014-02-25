433931b094468683785bc6c917411a12e58ba334
joel <joel@Elizabeth.(none)>
2014-02-24 19:04:24 -0600

    commiting file

--- main.c
+++ main.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
+void cd();
+void dir();
+void clr();
+void environ();
+void echo();
+void help();
+void pause();
+void quit();
+
 int main(void)
 {
     string program;
+    while(true){
     progam < cin;
 
-    switch(program){
-    case "cd":
-        break;
-    case "dir":
-        break;
-    case "clr":
-        break;
-    case "environ":
-        break;
-    case "echo":
-        break;
-    case "help":
-        break;
-    case "pause":
-        break;
-    case "quit":
-        break;
+        switch(program){
+        case "cd":
+            break;
+        case "dir":
+            break;
+        case "clr":
+            break;
+        case "environ":
+            break;
+        case "echo":
+            break;
+        case "help":
+            break;
+        case "pause":
+            break;
+        case "quit":
+            break;
+        }
     }
 
-    printf("Hello World!\n");
-    return 0;
 }
 
